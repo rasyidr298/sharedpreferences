79047ce726b714f1519fef4d2ec52a6fb82ebbf3
rasyidr298 <rasyidridla298@gmail.com>
2019-10-30 19:32:33 +0700

    Init Commit

---
+++ app/src/main/java/com/example/sharedprefrences/helper/SharedPref.java
@@ -0,0 +1,53 @@
+package com.example.sharedprefrences.helper;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharedPref {
+
+    private static SharedPref INSTANCE;
+    private SharedPreferences sharedPreferences;
+
+
+    private final String SP_NAME = "loginsession";
+
+    public static final String SP_NAMA = "istLogin" ;
+
+    public SharedPref(Context context){
+        sharedPreferences = context
+                .getApplicationContext()
+                .getSharedPreferences(SP_NAME,Context.MODE_PRIVATE);
+    }
+
+
+    public static SharedPref getInstance(Context context){
+        if (INSTANCE == null){
+            INSTANCE = new SharedPref(context);
+        }
+        return INSTANCE;
+    }
+
+    public SharedPreferences Pref(){
+        return sharedPreferences;
+    }
+
+    public Boolean isLogin(){
+        return sharedPreferences.getBoolean("istLogin", false );
+    }
+
+    public void setLogin (boolean isCall){
+        sharedPreferences.edit().putBoolean("istLogin", isCall).apply();
+    }
+
+    public void setNama(String isNama){
+        sharedPreferences.edit().putString("isNama", isNama).apply();
+    }
+    public String getUsername() {
+        return sharedPreferences.getString("","admin");
+    }
+
+    public String getNamaku() {
+        return sharedPreferences.getString("","Rasyid Ridla");
+    }
+
+}
